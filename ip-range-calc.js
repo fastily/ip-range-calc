8f71d7c44dcd71dc6b388047c70e141c5af21f69
fastily <fastily@users.noreply.github.com>
2017-07-28 00:21:23 -0700

    initial commit, basic functionality works

---
+++ ip-range-calc.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+	var calcCIDR = function(ipL, cnt) {
+
+		var addrs = [];
+		for(i=0; i < ipL.length; i++)
+			addrs.push(ipaddr.parse(ipL[i]));
+
+		if(addrs.length > 1)
+			for(i = 1; i < addrs.length; i++)
+				while(!addrs[i].match(addrs[0], cnt) && cnt > 0)
+					cnt--;
+
+		return addrs[0].toString() + "/" + cnt;
+	};
+
+	$('#goButton').click(function(e) {
+		var boxText = $('#ipBox').val().trim();
+		if(!boxText)
+			return;
+
+		var cnt = $("#protocol-select option:selected").val();
+		var protocol = cnt == 32 ? ipaddr.IPv4 : ipaddr.IPv6;
+		var ipL = boxText.split("\n");;
+
+		for(i=0; i < ipL.length; i++)
+			if(!protocol.isValid(ipL[i]))
+			{
+				alert("Error: '" + ipL[i] + "' is not a valid IP address.  Please fix before proceeding");
+				return;
+			}
+
+		$('#resultBox').text(calcCIDR(ipL, cnt));
+	});
+});
